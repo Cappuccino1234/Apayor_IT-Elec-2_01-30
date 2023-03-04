f6c69a1edc0fa3cd95d86d5dee131bd7fb2e3cca
Admin <cz2128.delta65@gmail.com>
2023-03-04 18:11:19 +0800

    Inputs

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear);
@@ -31,5 +38,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-
-
